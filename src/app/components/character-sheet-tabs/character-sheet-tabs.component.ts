2443b36d7375b1f78e9566a0388989fc9c52ca5f
arhughes11 <arhughes@my.waketech.edu>
2022-07-15 08:54:34 -0400

    finished basic page for character sheets

--- src/app/components/character-sheet-tabs/character-sheet-tabs.component.ts
+++ src/app/components/character-sheet-tabs/character-sheet-tabs.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-character-sheet-tabs',
   templateUrl: './character-sheet-tabs.component.html',
-  styleUrls: ['./character-sheet-tabs.component.css']
+  styleUrls: ['./character-sheet-tabs.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CharacterSheetTabsComponent implements OnInit {
 
