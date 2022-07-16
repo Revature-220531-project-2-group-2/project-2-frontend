87e5bfb7bc36cea7f40c5e2813dd16928fa2c6c5
arhughes11 <arhughes@my.waketech.edu>
2022-07-15 20:46:14 -0400

    storing user in app.component after logging in

---
+++ src/app/components/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+
+@Component({
+  selector: 'app-user-dashboard',
+  templateUrl: './user-dashboard.component.html',
+  styleUrls: ['./user-dashboard.component.css']
+})
+export class UserDashboardComponent implements OnInit {
+
+  constructor(public appComponent: AppComponent) { }
+
+  ngOnInit(): void {
+  }
+
+}
